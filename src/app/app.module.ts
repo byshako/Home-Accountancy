8be610386e0ef77b10d2a6bba2940adf2f826206
byshako1 <sh.kavtaradze@adjarabet.com>
2017-12-18 18:28:06 +0400

    Register/login Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AuthModule,
+    AppRoutingModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
