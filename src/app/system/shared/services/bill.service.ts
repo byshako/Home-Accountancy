eca54695cf80576a38e96b7a9ffc2db7a7ef2885
byshako1 <sh.kavtaradze@adjarabet.com>
2017-12-25 13:43:24 +0400

    base API service (16)

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Bill } from '../models/bill.model';
+import {BaseApi} from '../../../shared/core/base-api';
 
 @Injectable()
-export class BillService{
-    constructor(private http: Http){}
+export class BillService extends BaseApi{
+    constructor(public http: Http){
+        super(http);
+    }
 
     getBill(): Observable<Bill> {
-        return this.http.get('http://localhost:3000/bill')
-            .map((response: Response) => response.json());
+        return this.get('bill');
     }
 
     getCurrency(base: string = 'RUB'): Observable<any> {
